f93d49d33d0faa5b54b5a5d2123489884fde9f86
Alex Au <45087631+Alex-Au1@users.noreply.github.com>
2022-12-30 02:34:18 -0500

    Delete data_source_loc.py

--- Reporter/data_sources/data_source_loc.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-
-# DatSourceLoc: All possible datasource locations
-class DataSourceLoc(enum.Enum):
-    Nav = "nav"
-    Sharepoint = "sharepoint"
-    Excel = "excel"
-    CustomTable = "custom table"
