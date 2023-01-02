4403d632d0c9017f5ef3c5c14517799c63d3d9aa
Alex Au <45087631+Alex-Au1@users.noreply.github.com>
2023-01-01 22:50:20 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PyReporter'
+copyright = '2023, Alex Au'
+author = 'Alex Au'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0.5
+version = '1.0.5
 
 # -- General configuration
 
